edb5266394dd53025294df594ca2ebb00fae04fb
Matheus <matheus.eai@gmail.com>
2023-05-02 21:46:35 -0300

    Pagination and generic classes

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -10,35 +12,42 @@ from ..models import Recipe
 from ..serializers import RecipeSerializer, TagSerializer
 
 
-class RecipeAPIv2List(APIView):
-    def get(self, request):
-        recipes = Recipe.objects.get_published()[:10]
+class RecipeAPIv2Pagination(PageNumberPagination):
+    page_size = 1
 
-        serializer = RecipeSerializer(
-            instance=recipes,
-            many=True,
-            context={
-                'request': request
-            },)
 
-        return Response(serializer.data)
+class RecipeAPIv2List(ListCreateAPIView):
+    queryset = Recipe.objects.get_published()
+    serializer_class = RecipeSerializer
+    pagination_class = RecipeAPIv2Pagination
 
-    def post(self, request):
-        dados = request.data
+    # def get(self, request):
+    #     recipes = Recipe.objects.get_published()[:10]
 
-        serializer = RecipeSerializer(
-            data=request.data,
-            context={'request': request}
-        )
+    #     serializer = RecipeSerializer(
+    #         instance=recipes,
+    #         many=True,
+    #         context={
+    #             'request': request
+    #         },)
 
-        serializer.is_valid(raise_exception=True)
-        serializer.save(
-            author_id=1,
-            category_id=1,
-            tags=[1, 2]
-        )
+    #     return Response(serializer.data)
 
-        return Response(serializer.data, status=status.HTTP_200_OK)
+    # def post(self, request):
+
+    #     serializer = RecipeSerializer(
+    #         data=request.data,
+    #         context={'request': request}
+    #     )
+
+    #     serializer.is_valid(raise_exception=True)
+    #     serializer.save(
+    #         author_id=1,
+    #         category_id=1,
+    #         tags=[1]
+    #     )
+
+    #     return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class RecipeAPIv2Detail(APIView):
@@ -58,6 +67,7 @@ class RecipeAPIv2Detail(APIView):
                 'request': request
             }
         )
+        return Response(serializer.data)
 
     def patch(self, request, pk):
         recipe = self.get_recipe(pk)
