4082e4074467ec78e20c991b392ca5bf96ad582e
Matheus <matheus.eai@gmail.com>
2023-05-03 17:32:11 -0300

    Authentication for token

--- project/settings/rest_framework.py
+++ project/settings/rest_framework.py
@@ -7,7 +7,10 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 20,
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    "DEFAULT_RENDERER_CLASSES": (
+        'rest_framework.renderers.JSONRenderer',
+    ),
 }
 
 
@@ -16,5 +19,5 @@ SIMPLE_JWT = {
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "BLACKLIST_AFTER_ROTATION": False,
     "SIGNING_KEY": os.environ.get('SECRET_KEY_JWT', 'INSECURE'),
-    "AUTH_HEADER_TYPES": ("Bearer",),
+    "AUTH_HEADER_TYPES": ("Bearer",)
 }
--- recipes/urls.py
+++ recipes/urls.py
@@ -61,15 +61,15 @@ urlpatterns = [
     ),
 
     path(
-        'api/token/',
+        'recipes/api/token/',
         TokenObtainPairView.as_view(),
         name='token_obtain_pair'),
     path(
-        'api/token/refresh/',
+        'recipes/api/token/refresh/',
         TokenRefreshView.as_view(),
         name='token_refresh'),
     path(
-        'api/token/verify/',
+        'recipes/api/token/verify/',
         TokenVerifyView.as_view(),
         name='token_verify'),
 
